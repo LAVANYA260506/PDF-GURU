7c8154d8c9060e60b54153d23bf331b09b36efc5
LAVANYA260506 <lavanyaanand857@gmail.com>
2026-08-19 18:29:16 +0530

    UPDATE : model changed from llama-3.1 to openai/120b

--- llm.py
+++ llm.py
@@ -6,7 +6,7 @@ from groq import Groq
 load_dotenv()
 
 groq_api_key = os.getenv("groq_api_key")
-MODEL = "llama-3.1-8b-instant"
+MODEL = "openai/gpt-oss-120b"
 
 SYSTEM_PROMPT =  """
 You are GURU — a calm, knowledgeable teacher who explains concepts in a natural, conversational way.
